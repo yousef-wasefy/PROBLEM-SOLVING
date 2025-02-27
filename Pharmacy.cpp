1b48628ab3ab5870d26e1e90eea293d9f8432f3d
Youssef <yousefalwasefy@gmail.com>
2025-02-27 17:29:15 +0200

    done

--- Pharmacy.cpp
+++ Pharmacy.cpp
@@ -5,20 +5,29 @@ using namespace std;
 
 struct Medicine {
     string name;  // اسم الدواء
+    string category;
     double price; // سعر الدواء
-    int stock;    // الكمية المتاحة
+    // int stock;    // الكمية المتاحة
+    
 };
 
-Medicine medicines[100]; // مصفوفة لتخزين 100 دواء كحد أقصى
+// Medicine medicines[100]; // مصفوفة لتخزين 100 دواء كحد أقصى
 
 struct CartItem {
     int medicineIndex; // رقم الدواء في المصفوفة
     int quantity;      // الكمية التي اختارها المستخدم
 };
 
-struct user {
-
+Medicine medicines[] = {
+    {"Paracetamol", "Painkiller", 5.0},
+    {"Ibuprofen", "Painkiller", 7.5},
+    {"Aspirin", "Painkiller", 6.0},
+    {"Amoxicillin", "Antibiotic", 10.0},
+    {"Azithromycin", "Antibiotic", 12.0},
+    {"Cough Syrup", "Cold & Flu", 8.0}
 };
+
+int totalMedicines = sizeof(medicines) / sizeof(medicines[0]);
 
 void searchForMedicine()
 {
@@ -26,38 +35,61 @@ void searchForMedicine()
     string search;
     string category;
     string med;
+    char add; //answer to add to the cart
 
-    cout << "Enter the search type (c for category/n for name)";
+    cout << "Enter the search type (by category/by name)";
     cin >> choice;
 
     switch(choice)
     {
         case 'n':
+        case 'N':
         cout << "search for medicine: ";
         cin >> search;
 
-        for (int i = 0; i < 100; i++){
-        if (medicines[i].name == search){
+        for (int i = 0; i < totalMedicines; i++){
+        if (medicines[i].name == search)
+        {
             cout << "Medicine : " << medicines[i].name << "\nPrice : " << medicines[i].price << endl;
-            cout << "In stock : " << medicines[i].stock << endl;
-            //addToCart(search);
+            // cout << "In stock : " << medicines[i].stock << endl;
+
+            // cout << "would you add it to the cart?(y/n)\n";
+            // cin >> add;
+            // if (add == 'y' || add == 'Y')
+            //     //addToCart(search);
+            // ;
         }
         }break;
 
         case 'c':
+        case 'C':
         cout << "which category you are looking for?" <<  endl;
         cin >> category;
-        //displayMedicines(category);
+        for (int i = 0; i < totalMedicines; i++)
+            if (medicines[i].category == category)
+            {
+                cout << "Medicine : " << medicines[i].name << "\nPrice : " << medicines[i].price << endl;
+                // cout << "In stock : " << medicines[i].stock << endl;
+                cout << "=========" << endl;
+            }
         cout << "Enter the medicine you want" << endl;
         cin >> med;
 
-        for (int i = 0; i < 100; i++){
-            if (medicines[i].name == med){
+        for (int i = 0; i < totalMedicines; i++)
+            if (medicines[i].name == med)
+            {
                 cout << "Medicine : " << medicines[i].name << "\nPrice : " << medicines[i].price << endl;
-                cout << "In stock : " << medicines[i].stock << endl;
-                //addToCart(med);
-            }
-            }
+                // cout << "In stock : " << medicines[i].stock << endl;
+
+                // cout << "would you add it to the cart?(y/n)\n";
+                // cin >> add;
+                // if (add == 'y' || add == 'Y')
+                //     //addToCart(med);
+                // ;
+            }break;
+        default:
+            cout << "Invalid choice. Please enter 'n' for name or 'c' for category." << endl;
+            break;
     }
 }
 
